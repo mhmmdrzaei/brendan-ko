174a10e5f4de11192de144f02ab5efd335ad4427
Mohammad Rezaei <mhmmdrzaei@gmail.com>
2025-04-20 14:42:14 -0400

    should work

--- app/components/layout.jsx
+++ app/components/layout.jsx
@@ -1,12 +1,13 @@
 
-import Header from './Header';
 import Footer from './Footer';
+import Header from './Header';
 import { getsettings } from '@/sanity/sanity.utils';
 
 export default async function Layout({ children }) {
   const settings = await getsettings()
   return (
     <>
+    
       <Header settings={settings} />
       <main>{children}</main>
       <Footer settings={settings} />

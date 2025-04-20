e7fe05c26b5122bcae7eae123433bd6ef5670f41
Mohammad Rezaei <mhmmdrzaei@gmail.com>
2025-04-20 14:45:56 -0400

    mm

--- app/components/layout.jsx
+++ app/components/layout.jsx
@@ -1,6 +1,6 @@
 
 import Footer from './Footer';
-import Header from './Header';
+import Header from '@/app/components/header'
 import { getsettings } from '@/sanity/sanity.utils';
 
 export default async function Layout({ children }) {
